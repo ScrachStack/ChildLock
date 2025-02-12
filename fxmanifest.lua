5c21d74d4c492e0d1c6a2bfd052a13536c63819f
ScrachStack <122732007+ScrachStack@users.noreply.github.com>
2025-02-12 21:04:24 +0000

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+---------------------------------
+--- Child Lock, Made by FAXES ---
+---------------------------------
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Faxes'
+client_scripts {
+    'client.lua'
+}
+shared_script 'config.lua'

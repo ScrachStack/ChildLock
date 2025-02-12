2c06738507f82c1b627f5839050b30d813f7bda0
ScrachStack <122732007+ScrachStack@users.noreply.github.com>
2025-02-12 21:02:10 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,22 +1,31 @@
 ---------------------------------
 --- Child Lock, Made by FAXES ---
 ---------------------------------
-
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
         local ped = PlayerPedId()
         local veh = GetVehiclePedIsUsing(ped)
         --local veh GetVehiclePedIsIn(ped, false)
-        
         if IsPedInVehicle(ped, veh, false) then
             --if GetVehiclePedIsIn(ped, false) then
-                if GetVehicleClass(veh) == 18 then
-                    --print("class passed")
-                    if GetPedInVehicleSeat(veh, -1) ~= ped and GetPedInVehicleSeat(veh, 0) ~= ped then
-                        --print("ped is not in drivers seat")
-                        if IsVehicleSeatFree(veh, -1) then
-                            --print("drivers seat is free")
+            local vehModel = GetEntityModel(veh)
+            if table.contains(Settings.vehicles, vehModel) then
+                if Settings.Debug then 
+                    print("Vehicle model is in the allowed list")
+                end
+                if Settings.Debug then 
+                            print("class passed")
+                    end
+			if GetPedInVehicleSeat(veh, -1) ~= ped and GetPedInVehicleSeat(veh, 0) ~= ped then
+			                if Settings.Debug then 		
+                            print("ped is not in drivers seat")
+			                end
+                            if IsVehicleSeatFree(veh, -1) then
+			                if Settings.Debug then 		
+			                print("drivers seat is free")
+
+							end
                             DisableControlAction(0, 75, true) -- exit veh
                             if IsDisabledControlJustPressed(0, 75) then
                                 TriggerEvent("chatMessage", "^1^*Vehicle child lock is active.")
@@ -24,7 +33,15 @@ Citizen.CreateThread(function()
                         end
                     end
                 end
-            --end
         end
 	end
 end)
+
+function table.contains(table, value)
+    for _, v in ipairs(table) do
+        if v == value then
+            return true
+        end
+    end
+    return false
+end
